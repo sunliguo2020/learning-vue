f50ab362c20e8dd8906aae078b36d9eeadd6cb73
sunliguo <sunliguo2006@qq.com>
2023-10-20 23:29:53 +0800

    根据网上的教程，学习的Vue。 自己一个字母一个字母敲得。 再看网上教程的时候，没有资料。只有暂停视频，一段一段自己手动敲的。

---
+++ vue/Vue知识体系/chap01 Vue基础/源码/vue-components/src/components/mycomponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">组件基础组成</div>
+</template>
+<script >
+export default {
+  data() {
+    return "";
+  },
+};
+</script>
+
+
+<!--scoped 让当前样式只在当前组件生效-->
+<style scoped>
+.container {
+  font-size: 20px;
+}
+</style>
