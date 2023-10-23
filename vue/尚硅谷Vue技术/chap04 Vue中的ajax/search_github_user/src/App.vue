d4e68c70caa4fbe0513be521ea4721bc169e581a
sunliguo <sunliguo2006@qq.com>
2023-10-24 07:51:11 +0800

    作用域插槽

--- vue/尚硅谷Vue技术/chap04 Vue中的ajax/search_github_user/src/App.vue
+++ vue/尚硅谷Vue技术/chap04 Vue中的ajax/search_github_user/src/App.vue
@@ -1,34 +1,30 @@
 <template>
   <div class="container">
-    <category title="美食">
-      <img
-        slot="center"
-        src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg"
-        alt=""
-      />
-      <a slot="footer" href="">更多美食</a>
-    </category>
 
     <category title="游戏">
-      <ul slot="center">
-        <li v-for="(item, index) of games" :key="index">{{ item }}</li>
+      <template scope="atguigu">
+        <ul >
+        <li v-for="(item, index) of atguigu.games" :key="index">{{ item }}</li>
       </ul>
-      <div slot="footer" class="foot">
-        <a href="#">单机游戏</a>
-        <a href="#">网络游戏</a>
-      </div>
+      </template>
+     
     </category>
-    <category title="电影">
-      <video
-        slot="center" controls
-        src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
-      ></video>
-      <template v-slot:footer >
-        <a href="#">经典</a>
-        <a href="#">热门</a>
-        <a href="#">推荐</a>
+    <category title="游戏">
+
+      <template scope="{games}">
+        <ul >
+        <li style="color:red" v-for="(item, index) of games" :key="index">{{ item }}</li>
+      </ul>
       </template>
     </category>
+  
+    <category title="游戏">
+      <template slot-scope="{games}">
+        <h4 v-for="(item, index) of games" :key="index">{{ item }}</h4>
+      </template>
+    </category>
+  
+  
   </div>
 </template>
 
@@ -40,13 +36,7 @@ export default {
   components: {
     Category,
   },
-  data() {
-    return {
-      foods: ["火锅", "烧烤", "小龙虾", "牛排"],
-      games: ["红色警戒", "穿越火线", "劲舞团", "超级玛丽"],
-      films: ["《教父》", "《拆弹专家》", "《你好，李焕英》", "《尚硅谷》"],
-    };
-  },
+  
 };
 </script>
 <style>
