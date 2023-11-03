6873c0e274c0dd41d2e8e2d63e1bf13934de9cb2
sunliguo <sunliguo2006@qq.com>
2023-11-03 22:20:54 +0800

    Person添加搜索功能

--- VueProject/sgy-vue/src/pages/Person.vue
+++ VueProject/sgy-vue/src/pages/Person.vue
@@ -8,6 +8,15 @@
       <option :value="15">15</option>
       <option :value="20">20</option>
     </select>
+    <!-- 搜索 -->
+    <div class="search">
+            <el-input @change="searchInput" 
+                        v-model="searchData" 
+                        aria-placeholder="请输入内容"
+                        clearable
+                        ></el-input>
+            <el-button type="primary">查询</el-button>
+        </div>
     <el-table :data="tableData" style="width: 100%" :stripe="true">
       <el-table-column prop="id" label="ID" width="50"> </el-table-column>
       <el-table-column prop="name" label="姓名" width="100"> </el-table-column>
@@ -31,7 +40,8 @@ export default {
     return {
       tableData: [],
       total: 10,
-      pageSize: 20,
+      pageSize: 5,
+      searchData:'',
     };
   },
   watch: {
@@ -60,6 +70,15 @@ export default {
     changePage(num) {
       this.getPersons(num)
     },
+    searchInput(){
+      this.axios.get('http://127.0.0.1:8000/api/person/?search='+this.searchData).then(
+        (res)=>{
+          console.log("搜索请求成功了", res.data);
+          this.tableData = res.data.results;
+          this.total = res.data.count;
+        }
+      )
+    }
   },
   created() {
     this.getPersons(1);
