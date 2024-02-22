e7df313150fef8e4b4f5444d9eff733f01e77b77
sunliguo <sunliguo2006@qq.com>
2024-02-22 17:12:46 +0800

    uniapp学习

---
+++ uniapp/HBuilderProjects/unidemo3/env.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "is-custom-runtime": false,
+    "compile": true,
+    "host": "192.168.31.253",
+    "h5": {
+        "options": {
+            "headless": true
+        },
+        "executablePath": ""
+    },
+    "mp-weixin": {
+        "port": 9420,
+        "account": "",
+        "args": "",
+        "cwd": "",
+        "launch": true,
+        "teardown": "disconnect",
+        "remote": false,
+        "executablePath": ""
+    },
+    "app-plus": {
+        "android": {
+            "id": "",
+            "executablePath": ""
+        },
+        "version": "",
+        "ios": {
+            "id": "",
+            "executablePath": ""
+        },
+        "uni-app-x": {
+            "version": "C:\\Users\\hdx\\Downloads\\HBuilderX\\plugins\\uniappx-launcher\\base\\version.txt",
+            "android": {
+                "id": "9e21aa6e",
+                "executablePath": "C:\\Users\\hdx\\Downloads\\HBuilderX\\plugins\\uniappx-launcher\\base\\android_base.apk"
+            }
+        }
+    }
+}
---
+++ uniapp/HBuilderProjects/unidemo3/uni_modules/call-easy-method/components/call-easy-method-uni-modules/call-easy-method-uni-modules.vue
@@ -0,0 +1,26 @@
+<template>
+  <view></view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    methods: {
+      foo1() {
+      },
+      foo2(date1 : number) {
+      },
+      foo3(date1 : number, date2 : number) {
+      },
+      foo4(callback : (() => void)) {
+        callback()
+      },
+      foo5(text1 : string) : any | null {
+        return text1
+      }
+    }
+  }
+</script>
